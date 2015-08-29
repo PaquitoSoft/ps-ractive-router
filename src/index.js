191e803a7aff8a9babba8ffe16813bc1292e65e3
PaquitoSoft <paquitosoftware@gmail.com>
2015-08-29 19:06:58 +0200

    I need to export router so application sub-components can use the navigation api if needed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 
+module.exports.RouterManager = require('./router-manager');
 module.exports.BaseApp = require('./base-app');
 module.exports.BasePage = require('./components/base-page-component');
