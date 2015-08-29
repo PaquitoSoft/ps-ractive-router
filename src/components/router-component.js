cf6c7daf05cdc24d7468804d7a0abae63cefb943
PaquitoSoft <paquitosoftware@gmail.com>
2015-08-29 19:08:36 +0200

    Correctly export router component

--- src/components/router-component.js
+++ src/components/router-component.js
@@ -9,16 +9,23 @@ var Router = Ractive.extend({
 	template: '<router-handler/>',
 	components: {
 		'router-handler': function() {
+			console.log('RouterComponent::components::route-handler# component to be rendered:', this.get('componentName'));
 			return this.get('componentName');
 		}
 	},
 	oninit: function _oninit() {
 		this.observe('componentName', function(newValue, oldValue) {
+			console.log('RouterComponent::oninit# componentName changed!', newValue, oldValue);
 			if (this.fragment.rendered) {
 				this.reset();
+				console.log('RouterComponent::oninit# component reset!');
 			}
 		});
+		console.log('RouterComponent::oninit# component ready to be rendered:', this.get('componentName'));
+	},
+	onrender: function() {
+		console.log('Rendering router component...', this.get('componentName'));
 	}
 });
 
-module.exports.RouterComponent = Router;
+module.exports = Router;
