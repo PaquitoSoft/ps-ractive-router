cd14b04d7325a7805871854ecae704cc7d2e85b7
PaquitoSoft <paquitosoftware@gmail.com>
2015-10-04 17:27:49 +0200

    Change list file name

--- webpack.config.js
+++ webpack.config.js
@@ -4,32 +4,11 @@ var webpack = require('webpack');
 module.exports = {
 	entry: './src/index.js',
 	output: {
-		filename: './dist/js/psrr-bundle.js',
+		filename: './dist/ps-ractive-router.js',
 		libraryTarget: 'umd',
 		library: 'PSRR'
 	},
 	externals: {
 		ractive: 'Ractive'
 	}
-	/*,
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				loader: 'babel'
-			},
-			{
-				test: /\.html$/,
-				loader: 'raw'
-			}
-		]
-	},
-	// This is to load polyfills (http://mts.io/2015/04/08/webpack-shims-polyfills/)
-	plugins: [
-		new webpack.ProvidePlugin({
-			fetch: 'imports?this=>global!exports?global.fetch!whatwg-fetch',
-			'es6-promise': 'es6-promise'
-		})
-	]*/
 };
