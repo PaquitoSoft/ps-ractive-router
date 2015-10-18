dca81b1d948cfabb193e53127b24e1b49ec627a8
PaquitoSoft <paquitosoftware@gmail.com>
2015-10-19 00:02:44 +0200

    Added dist files

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,27 @@
 /* global module, require */
 var webpack = require('webpack');
 
+var isBundleForProduction = typeof process.env.BUNDLE_TARGET !== 'undefined';
+
+var plugins = [],
+	outputFileName = './dist/ps-ractive-router';
+
+if (isBundleForProduction) {
+	plugins.push(new webpack.optimize.UglifyJsPlugin({ minimize: true }));
+	outputFileName += '.min.js';
+} else {
+	outputFileName += '.js';
+}
+
 module.exports = {
 	entry: './src/index.js',
 	output: {
-		filename: './dist/ps-ractive-router.js',
+		filename: outputFileName,
 		libraryTarget: 'umd',
 		library: 'PSRR'
 	},
 	externals: {
 		ractive: 'Ractive'
-	}
+	},
+	plugins: plugins
 };
